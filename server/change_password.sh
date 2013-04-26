6c7deefbe10a0ef38a24be1a25d7045997c2782b
jiachenz <jiachenz@usc.edu>
2013-04-25 20:44:57 -0700

    Modified the script for changing the password

--- server/change_password.sh
+++ server/change_password.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 user="root"
-pass="rootmysql"
-db="users"
+db="ctf2"
 host="localhost"
-mysql -u "$user" -p "$pass" -h $host -D $db -e "UPDATE 'users' SET 'pass'='3X4T5V6789asd' WHERE pass='abcdef'" | while read pass; do
-	echo "changing password:"
-done
-	
+mysql -uroot -p -e"
+ use ctf2;
+ UPDATE users SET pass='3X4T5V6789asd' WHERE pass='abcdef';
+"
