1f7546f21e0d7b2bed401a8addbff1ccc24a5872
rpuncel <puncel@usc.edu>
2013-04-26 01:31:17 -0700

    Much simplified, simple firewall file.  It is currently deployed on gateway in ctf experiment

--- gateway/firewall.sh
+++ gateway/firewall.sh
@@ -1,8 +1,6 @@
+ROUTER_ETH="eth1"
 sudo iptables -F
 sudo iptables -X
 sudo iptables -A FORWARD -p tcp --dport 80 -m limit --limit 65/minute --limit-burst 100 -j ACCEPT
-sudo iptables -A FORWARD -p tcp --sport 80 -j ACCEPT
-sudo iptables -A INPUT -i eth1 -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
-sudo iptables -A OUTPUT -o eth1 -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
-sudo iptables -A INPUT -i lo -j ACCEPT
-sudo iptables -A OUTPUT -o lo -j ACCEPT
+sudo iptables -A FORWARD -i $ROUTER_ETH -j DROP
+sudo iptables -A INPUT -i $ROUTER_ETH -j DROP
